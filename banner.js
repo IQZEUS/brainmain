8606978296a35c646b831056ca5cec9ec277b17b
IQZEUS <amirhosseindelfan000@gmail.com>
2026-09-03 22:33:43 +0330

    Update banner.js via admin panel

--- banner.js
+++ banner.js
@@ -12,7 +12,7 @@ window.BANNER_DATA = {
   },
   "fa": {
     "title": "📢 نسخه جدید میااا000000000000000000000000000اااااااایییینتشر شد",
-    "sub": "مغز نسخه ۲ منتشر شد!",
+    "sub": "مغز نسخه ۲ منتشر شد!ذذذذذذذذ",
     "image": "https://cdn.schoolspecialty.com/56f76621-1f51-4a5b-9111-b0f1002a7860/586282_JPG%20Output.jpg",
     "content": "باذابذذبذ",
     "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"><img src=\"https://cdn.schoolspecialty.com/56f76621-1f51-4a5b-9111-b0f1002a7860/586282_JPG%20Output.jpg\" alt=\"banner\" style=\"max-width:100%;max-height:180px;object-fit:contain;border-radius:8px;\"><div style=\"color:#c9c6bd;font-size:14px;line-height:1.8;\">باذابذذبذ</div></div>",
