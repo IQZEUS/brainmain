c6a619b48b089439a0201fd16209d67922ffc158
IQZEUS <amirhosseindelfan000@gmail.com>
2026-09-03 22:37:02 +0330

    Update banner.js via admin panel

--- banner.js
+++ banner.js
@@ -2,17 +2,17 @@ window.BANNER_DATA = {
   "en": {
     "title": "📢 New Version Available",
     "sub": "Second Brain v2.0 is here!",
-    "image": "",
+    "image": "https://cdn.schoolspecialty.com/56f76621-1f51-4a5b-9111-b0f1002a7860/586282_JPG%20Output.jpg",
     "content": "",
-    "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"></div>",
+    "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"><img src=\"https://cdn.schoolspecialty.com/56f76621-1f51-4a5b-9111-b0f1002a7860/586282_JPG%20Output.jpg\" alt=\"banner\" style=\"max-width:100%;max-height:180px;object-fit:contain;border-radius:8px;\"></div>",
     "button": {
-      "label": "📥 Downlییییاااااااااااااااا00000000000000000000اااااااااااااااایoad Now",
+      "label": "📥 Downlیییoad Now",
       "link": "https://github.com/IQZEUS/brainmain"
     }
   },
   "fa": {
-    "title": "📢 نسخه جدید میااا000000000000000000000000000اااااااایییینتشر شد",
-    "sub": "مغزززززززززززززززززززززززززززززززززززززززززززززززززززز",
+    "title": "📢 نسخه جدید میاییینتشر شد",
+    "sub": "مغز ابدی",
     "image": "https://cdn.schoolspecialty.com/56f76621-1f51-4a5b-9111-b0f1002a7860/586282_JPG%20Output.jpg",
     "content": "باذابذذبذ",
     "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"><img src=\"https://cdn.schoolspecialty.com/56f76621-1f51-4a5b-9111-b0f1002a7860/586282_JPG%20Output.jpg\" alt=\"banner\" style=\"max-width:100%;max-height:180px;object-fit:contain;border-radius:8px;\"><div style=\"color:#c9c6bd;font-size:14px;line-height:1.8;\">باذابذذبذ</div></div>",
