a861771e3af757cebfc98a1f296a0594ebf32524
IQZEUS <amirhosseindelfan000@gmail.com>
2026-09-18 16:29:33 +0330

    Update banner.js via admin panel

--- banner.js
+++ banner.js
@@ -13,9 +13,9 @@ window.BANNER_DATA = {
   "fa": {
     "title": "📢 نسخه جدید میانتشر شد",
     "sub": "مغز ابدی",
-    "image": "https://www.beytoote.com/images/stories/fun/sky-profile-picture-2.jpg",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZlyl_MHGVTfuN5G6kwBPAhIFv7vOXGqo5-_sccBC8hg&s=10",
     "content": "باذابذذبذ",
-    "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"><img src=\"https://www.beytoote.com/images/stories/fun/sky-profile-picture-2.jpg\" alt=\"banner\" style=\"max-width:100%;max-height:180px;object-fit:contain;border-radius:8px;\"><div style=\"color:#c9c6bd;font-size:14px;line-height:1.8;\">باذابذذبذ</div></div>",
+    "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"><img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZlyl_MHGVTfuN5G6kwBPAhIFv7vOXGqo5-_sccBC8hg&s=10\" alt=\"banner\" style=\"max-width:100%;max-height:180px;object-fit:contain;border-radius:8px;\"><div style=\"color:#c9c6bd;font-size:14px;line-height:1.8;\">باذابذذبذ</div></div>",
     "button": {
       "label": "📥 دانلود نسخه جدید",
       "link": "https://github.com/IQZEUS/brainmain000000"
