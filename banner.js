3cfa81d3bf012aefa36f9cab87761ca6d4570f66
IQZEUS <amirhosseindelfan000@gmail.com>
2026-09-18 16:24:06 +0330

    Update banner.js via admin panel

--- banner.js
+++ banner.js
@@ -2,6 +2,8 @@ window.BANNER_DATA = {
   "en": {
     "title": "📢 New Version Available",
     "sub": "Second Brain v2.0 is here!",
+    "image": "",
+    "content": "",
     "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"></div>",
     "button": {
       "label": "📥 Downlییییاااااااااااااااا00000000000000000000اااااااااااااااایoad Now",
@@ -11,7 +13,9 @@ window.BANNER_DATA = {
   "fa": {
     "title": "📢 نسخه جدید میانتشر شد",
     "sub": "مغز ابدی",
-    "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"><img src=\"https://cdn.schoolspecialty.com/56f76621-1f51-4a5b-9111-b0f1002a7860/586282_JPG%20Output.jpg\" alt=\"banner\" style=\"max-width:100%;max-height:180px;object-fit:contain;border-radius:8px;\"><div style=\"color:#c9c6bd;font-size:14px;line-height:1.8;\">باذابذذبذ</div></div>",
+    "image": "https://www.beytoote.com/images/stories/fun/sky-profile-picture-2.jpg",
+    "content": "باذابذذبذ",
+    "html": "<div style=\"display:flex;flex-direction:column;align-items:center;gap:10px;\"><img src=\"https://www.beytoote.com/images/stories/fun/sky-profile-picture-2.jpg\" alt=\"banner\" style=\"max-width:100%;max-height:180px;object-fit:contain;border-radius:8px;\"><div style=\"color:#c9c6bd;font-size:14px;line-height:1.8;\">باذابذذبذ</div></div>",
     "button": {
       "label": "📥 دانلود نسخه جدید",
       "link": "https://github.com/IQZEUS/brainmain000000"
